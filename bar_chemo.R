e77ce36507fcfa2572bd7e1e11507e5d43cea858
omahern <94195915+omahern@users.noreply.github.com>
2023-02-10 10:48:47 -0500

    Update bar_chemo.R

--- bar_chemo.R
+++ bar_chemo.R
@@ -60,7 +60,7 @@ make_bar_relabun <- function(df, selection){
           strip.text.x = element_text(
             size = 10, color = "black", face = "bold"),
           strip.background = element_rect(
-            color="gray80", fill="gray97", size=1, linetype="solid"),
+            color="gray70", fill="white", size=1, linetype="solid"),
           panel.spacing = unit(0.05, "lines")
     )
 }
@@ -70,7 +70,7 @@ colls=c('gray50',"#a44f9a")
 m2=make_bar_relabun(qpcr_sub, "MC2")
 mm=make_bar_relabun(qpcr_sub, "MC2")
 
-colls=c('gray50',"#a44f9a")
+colls=c('gray50',"#6870c8")
 m3=make_bar_relabun(qpcr_sub, "MC3")
 colls=c('gray50',"#56ae6c")
 m4=make_bar_relabun(qpcr_sub, "MC4")
@@ -82,12 +82,14 @@ m6=make_bar_relabun(qpcr_sub, "MC6")
 lab1=c( "MC2-Met", "MC3-Eth", "MC4-Ace",
        "MC5-Glu", "MC6-Xyl")
 
-col1=c("#a44f9a","#a44f9a","#56ae6c","#af953c","#ba4a4f")
+col1=c("#a44f9a","#6870c8","#56ae6c","#af953c","#ba4a4f")
 
 library(cowplot)
 pdf(file='bd_data_chemo.pdf', width=12,height=12)
 plot_grid(m2,m3,m4,m5,m6,aligh='hv',ncol=1)
 dev.off()
+
+
 
 library(cowplot)
 pdf(file='bd_data_chemo3.pdf', width=12,height=12)
@@ -150,10 +152,13 @@ make_bar_relabun2 <- function(df, selection){
           strip.text.x = element_text(
             size = 10, color = "black", face = "bold"),
           strip.background = element_rect(
-            color="gray80", fill="gray97", size=1, linetype="solid"),
+            color="gray99", fill="gray99", size=1, linetype="solid"),
           panel.spacing = unit(0.05, "lines")
     )
 }
+
+
+
 
 
 
@@ -162,19 +167,54 @@ m2=make_bar_relabun1(qpcr_sub, "MC2")
 m2
 
 colls=c('gray50',"#6870c8")
-m3=make_bar_relabun1(qpcr_sub, "MC3")
+m3=make_bar_relabun2(qpcr_sub, "MC3")
+m3
 colls=c('gray50',"#56ae6c")
-m4=make_bar_relabun1(qpcr_sub, "MC4")
+m4=make_bar_relabun2(qpcr_sub, "MC4")
 colls=c('gray50',"#af953c")
-m5=make_bar_relabun1(qpcr_sub, "MC5")
+m5=make_bar_relabun2(qpcr_sub, "MC5")
 colls=c('gray50',"#ba4a4f")
 m6=make_bar_relabun2(qpcr_sub, "MC6")
 
 library(cowplot)
-pdf(file='bd_data_chemo2.pdf', width=8,height=12)
+#pdf(file='bd_data_chemo2.pdf', width=8,height=12)
 plot_grid(m2,m3,m4,m5,m6,aligh='hv',ncol=1)
 dev.off()
 
 
+lala <- function(df, selection){
+  df_out <- df %>% 
+    filter(MC %in% c(selection))}
+c12=subset(qpcr_sub, MC=="MC1")
+met=subset(qpcr_sub, MC=="MC2")
 
+lala=subset(qpcr_sub, MC!="MC1")
+colls=c('gray50',"#a44f9a","#6870c8","#56ae6c","#af953c","#ba4a4f")
 
+gg1=ggplot() + 
+  geom_line(data = c12, aes(x = Buckley, y = qPCR_RMAX, colour=MC)) +
+  geom_point(data = c12, aes(x = Buckley, y = qPCR_RMAX, colour=MC), size=1.4) +
+  geom_line(data = lala, aes(x = Buckley, y = qPCR_RMAX, colour=MC)) +
+  geom_point(data = lala, aes(x = Buckley, y = qPCR_RMAX, colour=MC), size=1.4) +
+  scale_fill_manual(values=c('gray50',"#a44f9a","#6870c8","#56ae6c","#af953c","#ba4a4f")) +
+  scale_colour_manual(values=c('gray50',"#a44f9a","#6870c8","#56ae6c","#af953c","#ba4a4f")) +
+  facet_grid(~TP, scale='free_x', space="free", shrink=TRUE) +
+  #geom_point(aes(colour=colls, 
+  #               fill = colls), size=1.3) + 
+  #geom_line(aes(colour=colls)) + 
+  labs(x="Buoyant Density (g/mL)", y = "Ratio of Maximum Quantity") +
+  theme_bw() +  scale_x_continuous(limits = c(1.76, 1.83)) +
+  theme(axis.text = element_text(color ='black',size=8, hjust=1,vjust=1),
+        axis.text.x=element_text(size=8, angle=90),
+        axis.title = element_text(color='black',face='bold',size=8),
+        legend.position = "right",
+        panel.grid=element_blank(),
+        legend.text = element_text(size=8),
+        legend.key.size = unit(0.25,'cm'),
+        strip.text.x = element_text(
+          size = 10, color = "black", face = "bold"),
+        strip.background = element_rect(
+          color="gray99", fill="gray99", size=1, linetype="solid"),
+        panel.spacing = unit(0.05, "lines"))
+gg1
+        
